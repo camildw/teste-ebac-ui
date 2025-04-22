0c20223839601d97a43d3a69fba39c206b6a0f85
camildw <camila.ldw@gmail.com>
2025-04-21 22:03:39 -0300

    Faker e variáveis

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -8,8 +8,8 @@ describe('Funcionalidade Cadastro', () => {
     });
 
    // afterEach(() => {
-  //      cy.screenshot()
-    //        });
+   //      cy.screenshot()
+  //        });
 
     it('Deve completar o cadastro com sucesso', () => {
         cy.get('#reg_email').type(faker.internet.email())
@@ -23,6 +23,25 @@ describe('Funcionalidade Cadastro', () => {
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados')
     
     });
+
+
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+        var nome = faker.person.firstName()
+        var email = faker.internet.email(nome)
+        var sobrenome = faker.person.lastName()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('teste@123')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados')
+    
+    });
+   
    
 
 })
