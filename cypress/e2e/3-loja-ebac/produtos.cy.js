7003732301c1ef7dd05f212c69e0b9da7863034b
camildw <camila.ldw@gmail.com>
2025-04-21 22:21:06 -0300

    Teste em lista de produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress"/>
+//import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos')
+    });
+
+        it('Deve selecionar um produto da lista', () => {
+
+            cy.get('.products > .row')
+                //.first()
+                //.last()
+                //.eq(2)
+                .contains('Apollo Running Short')
+                .click()
+
+            
+        });
+
+
+
+})
